9564327da5b9930a57cb0e2d0c4764e15e1f956f
Juan Pablo R <jprscs@gmail.com>
2020-08-02 21:05:47 -0500

    Fix static files

--- src/server.js
+++ src/server.js
@@ -25,6 +25,7 @@ mongoose.connect('mongodb://localhost/final-proyect')
 // App middlewares
 app.set('view-engine', 'ejs')
 app.set('views', path.join(__dirname,'views'))
+app.use(express.static(__dirname + '/public'))
 app.use(express.urlencoded({extended: false}))
 app.use(flash())
 app.use(session({
